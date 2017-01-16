974b555cc1d33636c91abbae5d251d8c9f34111d
David Martin <david.martin@redhat.com>
2017-01-16 15:56:36 +0000

    Remove undeterministic test assertions

--- test/test_log.js
+++ test/test_log.js
@@ -6,19 +6,6 @@ require('mocha-sinon');
 var logger = new consolelogger.ConsoleLogger();
 
 
-function getTS() {
-  var d = new Date();
-  var day = d.getDate() < 10 ? "0" + d.getDate() : d.getDate();
-  var mon = d.getMonth() < 10 ? "0" + d.getMonth() : d.getMonth();
-  var hour = d.getHours() < 10 ? "0" + d.getHours() : d.getHours();
-  var min = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
-  var sec = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
-
-  return d.getFullYear() + "-" + mon + "-" + day +
-    " " + hour + ":" + min + ":" + sec;
-}
-
-
 module.exports = {
   beforeEach: function() {
     var log = console.log;
@@ -30,52 +17,52 @@ module.exports = {
   'test error log level 0': function(done) {
     logger.setLogLevel(consolelogger.LEVEL.ERROR);
     logger.error('error');
-    expect(console.log.calledWith(getTS() + ' ERROR error')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' ERROR error'))).to.be.true;
     logger.warning('warning');
-    expect(console.log.calledWith(getTS() + ' WARN warning')).to.be.false;
+    expect(console.log.calledWith(sinon.match(' WARN warning'))).to.be.false;
     logger.info('info');
-    expect(console.log.calledWith(getTS() + ' INFO info')).to.be.false;
+    expect(console.log.calledWith(sinon.match(' INFO info'))).to.be.false;
     logger.debug('debug');
-    expect(console.log.calledWith(getTS() + ' DEBUG debug')).to.be.false;
+    expect(console.log.calledWith(sinon.match(' DEBUG debug'))).to.be.false;
     done();
   },
 
   'test error log level 1': function(done) {
     logger.setLogLevel(consolelogger.LEVEL.WARN);
     logger.error('error');
-    expect(console.log.calledWith(getTS() + ' ERROR error')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' ERROR error'))).to.be.true;
     logger.warning('warning');
-    expect(console.log.calledWith(getTS() + ' WARN warning')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' WARN warning'))).to.be.true;
     logger.info('info');
-    expect(console.log.calledWith(getTS() + ' INFO info')).to.be.false;
+    expect(console.log.calledWith(sinon.match(' INFO info'))).to.be.false;
     logger.debug('debug');
-    expect(console.log.calledWith(getTS() + ' DEBUG debug')).to.be.false;
+    expect(console.log.calledWith(sinon.match(' DEBUG debug'))).to.be.false;
     done();
   },
 
   'test error log level 2': function(done) {
     logger.setLogLevel(consolelogger.LEVEL.INFO);
     logger.error('error');
-    expect(console.log.calledWith(getTS() + ' ERROR error')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' ERROR error'))).to.be.true;
     logger.warning('warning');
-    expect(console.log.calledWith(getTS() + ' WARN warning')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' WARN warning'))).to.be.true;
     logger.info('info');
-    expect(console.log.calledWith(getTS() + ' INFO info')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' INFO info'))).to.be.true;
     logger.debug('debug');
-    expect(console.log.calledWith(getTS() + ' DEBUG warning')).to.be.false;
+    expect(console.log.calledWith(sinon.match(' DEBUG warning'))).to.be.false;
     done();
   },
 
   'test error log level 3': function(done) {
     logger.setLogLevel(consolelogger.LEVEL.DEBUG);
     logger.error('error');
-    expect(console.log.calledWith(getTS() + ' ERROR error')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' ERROR error'))).to.be.true;
     logger.warning('warning');
-    expect(console.log.calledWith(getTS() + ' WARN warning')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' WARN warning'))).to.be.true;
     logger.info('info');
-    expect(console.log.calledWith(getTS() + ' INFO info')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' INFO info'))).to.be.true;
     logger.debug('debug');
-    expect(console.log.calledWith(getTS() + ' DEBUG debug')).to.be.true;
+    expect(console.log.calledWith(sinon.match(' DEBUG debug'))).to.be.true;
     done();
   }
 };

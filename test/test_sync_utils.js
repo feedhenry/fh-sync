1f6ea8d2e8ee2c9a955adaef2807172d3db1496a
David Martin <david.martin@redhat.com>
2017-01-16 15:08:29 +0000

    Convert extra sync tests to 'exports' and add to 'unit' test run

--- test/test_sync_utils.js
+++ test/test_sync_utils.js
@@ -1,33 +1,28 @@
-'use strict';
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var mod;
 
-describe('sync utils', function () {
-
-  var sinon = require('sinon');
-  var expect = require('chai').expect;
-  var mod;
-
-  beforeEach(function () {
+module.exports = {
+  beforeEach: function () {
     // Clear all require cache to ensure old datasets etc. aren't hanging about
     require('clear-require').all();
 
     // Creates a new sync instance
-    mod = require('lib/sync-util');
-  });
+    mod = require('../lib/sync-util');
+  },
 
-  describe('#ensureHandlerIsFunction', function () {
-    it('should throw an AssertionError if not passed a function', function () {
+  '#ensureHandlerIsFunction': {
+    'should throw an AssertionError if not passed a function': function () {
       expect(function () {
         mod.ensureHandlerIsFunction('listHandler', {});
       }).to.throw(
         'AssertionError: sync handler (listHandler) must be a function'
-      );
-    });
-
-    it('should not throw an AssertionError if passed a function', function () {
+        );
+    },
+    'should not throw an AssertionError if passed a function': function () {
       expect(function () {
         mod.ensureHandlerIsFunction('listHandler', sinon.spy());
       }).to.not.throw();
-    });
-  });
-
-});
+    }
+  }
+};

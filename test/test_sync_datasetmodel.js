d8281ba141238a8f05b465952a604583259bd5ee
Evan Shortiss <evanshortiss@gmail.com>
2017-02-16 10:53:05 -0800

    support custom hashing rules for sync records

--- test/test_sync_datasetmodel.js
+++ test/test_sync_datasetmodel.js
@@ -13,7 +13,8 @@ var map = {
   'setGlobalCollisionLister': 'globalCollisionLister',
   'setGlobalCollisionRemover': 'globalCollisionRemover',
   'setGlobalRequestInterceptor': 'globalRequestInterceptor',
-  'setGlobalResponseInterceptor': 'globalResponseInterceptor'
+  'setGlobalResponseInterceptor': 'globalResponseInterceptor',
+  'setGlobalHashHandler': 'globalHashHandler'
 };
 
 var tests = {
@@ -34,12 +35,105 @@ var tests = {
       expect(customInterceptor.called).to.be.true;
       done();
     });
+  },
+
+  'should use the default hash handler': function () {
+    var records = {
+      'rec1': {
+        name: 'fh.sync'
+      },
+      'rec2': {
+        name: 'fh.cloud'
+      }
+    };
+
+    mod.generateRecordHashes('dataset_name', records, function (err, res) {
+      expect(err).to.be.null;
+      expect(res.hash).to.be.a('string');
+
+      expect(res.records.rec1.data.name).to.equal(records.rec1.name);
+      expect(res.records.rec1.hash).to.be.a('string');
+
+      expect(res.records.rec2.data.name).to.equal(records.rec2.name);
+      expect(res.records.rec2.hash).to.be.a('string');
+    });
+  },
+
+  'should use dataset specific hash handler': function () {
+    var records = {
+      'rec1': {
+        name: 'fh.sync'
+      },
+      'rec2': {
+        name: 'fh.cloud'
+      }
+    };
+
+    var expectedHash = '1234567890';
+
+    function hasher (/* record */) {
+      // Obviously a terrible idea...
+      return expectedHash;
+    }
+
+    mod.getDataset('dataset_name', function (err, dataset) {
+      // mimic setting the handler via sync api
+      dataset.hashHandler = hasher;
+
+      mod.generateRecordHashes('dataset_name', records, function (err, res) {
+        expect(err).to.be.null;
+        expect(res.hash).to.be.a('string');
+        expect(res.records).to.deep.equal({
+          rec1: {
+            data: records.rec1,
+            hash: expectedHash
+          },
+          rec2: {
+            data: records.rec2,
+            hash: expectedHash
+          }
+        });
+      });
+    });
+  },
+
+  'should return a global hash, and individual hashes using "globalHashHandler"': function () {
+    var records = {
+      'rec1': {
+        name: 'fh.sync'
+      },
+      'rec2': {
+        name: 'fh.cloud'
+      }
+    };
+
+    function hasher (record) {
+      // Obviously a terrible idea, but proves the concept
+      return JSON.stringify(record);
+    }
+
+    mod.setGlobalHashHandler(hasher);
+
+    mod.generateRecordHashes('dataset_name', records, function (err, res) {
+      expect(err).to.be.null;
+      expect(res.hash).to.be.a('string');
+      expect(res.records).to.deep.equal({
+        rec1: {
+          data: records.rec1,
+          hash: hasher(records.rec1) // should match generated hash if our handler was used
+        },
+        rec2: {
+          data: records.rec2,
+          hash: hasher(records.rec2) // should match generated hash if our handler was used
+        }
+      });
+    });
   }
 };
 
 Object.keys(map).forEach(function (fnName) {
   var handlerTests = {}
-  
+
   handlerTests['should throw an AssertionError due invalid argument'] = function () {
     expect(function () {
       mod[fnName](null)
@@ -55,7 +149,7 @@ Object.keys(map).forEach(function (fnName) {
     }).to.not.throw();
   }
 
-  tests['#' + fnName] = handlerTests;  
+  tests['#' + fnName] = handlerTests;
 });
 
 module.exports = tests;

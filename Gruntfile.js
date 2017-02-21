8fe98a3a451d7c82a791f70bee3d15937ced5890
Wei Li <wei.li@redhat.com>
2017-02-21 18:09:55 +0000

    add ack processor

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,8 @@ module.exports = function(grunt) {
     './test/sync/test_index.js',
     './test/sync/test_worker.js',
     './test/sync/test_sync-processor.js',
-    './test/sync/test_sync-scheduler.js'
+    './test/sync/test_sync-scheduler.js',
+    './test/sync/test_ack-processor.js'
   ];
   var unit_args = _.map(tests, makeTestArgs);
   var test_runner = '_mocha';

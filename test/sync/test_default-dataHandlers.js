fbfb49d8bf270181b9a939c433a0b516bd226cd6
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-02-27 13:53:12 +0100

    add implementation for default dataHandler
    Motivation:
    We need to have a default implementation of the dataHandler for cases
    where a user does not provide an override.
    
    Modifications:
    Added the default dataHandler implementation and updated the
    dataHandlersModule to take the defaults as its options so that the
    defaults will always be used unless overridden.
    
    JIRA:
    https://issues.jboss.org/browse/FH-3177

---
+++ test/sync/test_default-dataHandlers.js
@@ -0,0 +1,178 @@
+var assert = require('assert');
+var sinon = require('sinon');
+var defaultDataHandlersModule = require('../../lib/sync/default-dataHandlers.js');
+
+var id = 'datahandler_test';
+var queryParams = {};
+var metaData = {};
+
+// stubs when there is no real MongoDB
+var collectionStub = {
+  insertOne: sinon.stub().callsArgWith(1, null, {
+    result: { ok: 1, n: 1},
+    connection: null,
+    insertedCount: 1,
+    insertedId: '58b3d9efde2810043a0ac99d'}),
+  find: sinon.stub().returns({
+    toArray: sinon.stub().callsArgWith(0, null, [{
+      "_id": '58b3d9efde2810043a0ac99d'}]),
+  }),
+  findOne: sinon.stub().callsArgWith(1, null, {
+    "_id": '58b3d9efde2810043a0ac99d'
+  }),
+  update: sinon.stub().callsArgWith(2, null),
+  remove: sinon.stub().callsArgWith(1, null, {
+    result: { ok: 1, n: 1}
+  }),
+  drop: sinon.stub()
+};
+var dbStub = {
+  collection: sinon.stub().withArgs(id).returns(collectionStub)
+};
+
+var url = 'mongodb://localhost:27017/dataHandlersTest';
+var MongoClient = require('mongodb').MongoClient;
+
+module.exports = {
+
+  'test defaultDataHandlers module constructor': function(done) {
+    assert.throws(function() {
+      defaultDataHandlersModule();
+    }, function(err) {
+      assert.equal(err.message, 'MongoDB instance must be passed to module.');
+      done();
+      return true;
+    });
+  },
+
+  'test doCreate': function(done) {
+    MongoClient.connect(url, function(err, db) {
+      db = err ? dbStub : db;
+      var dataHandlers = defaultDataHandlersModule(db);
+      dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
+        assert.ok(!err);
+        assert.equal(res.result.ok, 1);
+        assert.equal(res.insertedCount, 1);
+        assert.ok(res.insertedId);
+        db.collection(id).drop();
+        done();
+      });
+    });
+  },
+
+  'test doList': function(done) {
+    const that = this;
+    MongoClient.connect(url, function(err, db) {
+      db = err ? dbStub : db;
+      var dataHandlers = defaultDataHandlersModule(db);
+      dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
+        var uid = res.insertedId;
+        dataHandlers.doList(id, queryParams, metaData, function(err, res) {
+          assert.ok(!err);
+          assert.ok(res[0]);
+          db.collection(id).drop();
+          done();
+        });
+      });
+    });
+  },
+
+  'test doRead': function(done) {
+    const that = this;
+    MongoClient.connect(url, function(err, db) {
+      db = err ? dbStub : db;
+      var dataHandlers = defaultDataHandlersModule(db);
+      dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
+        var uid = res.insertedId;
+        dataHandlers.doRead(id, uid, metaData, function(err, res) {
+          assert.ok(!err);
+          assert.ok(res._id);
+          db.collection(id).drop();
+          done();
+        });
+      });
+    });
+  },
+
+  'test doUpdate': function(done) {
+    const that = this;
+    MongoClient.connect(url, function(err, db) {
+      db = err ? dbStub : db;
+      var dataHandlers = defaultDataHandlersModule(db);
+      dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
+        var uid = res.insertedId;
+        dataHandlers.doUpdate(id, uid, {name: "Doe"}, metaData, function(err) {
+          assert.ok(!err);
+          db.collection(id).drop();
+          done();
+        });
+      });
+    });
+  },
+
+  'test doDelete': function(done) {
+    const that = this;
+    MongoClient.connect(url, function(err, db) {
+      db = err ? dbStub : db;
+      var dataHandlers = defaultDataHandlersModule(db);
+      dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
+        var uid = res.insertedId;
+        dataHandlers.doDelete(id, uid, metaData, function(err, res) {
+          assert.ok(!err);
+          db.collection(id).drop();
+          done();
+        });
+      });
+    });
+  },
+
+  'test handleCollision': function(done) {
+    const that = this;
+    MongoClient.connect(url, function(err, db) {
+      db = err ? dbStub : db;
+      var dataHandlers = defaultDataHandlersModule(db);
+      dataHandlers.handleCollision(id, metaData, {'name': 'Doe'}, function(err, res) {
+        assert.ok(!err);
+        assert.equal(res.result.ok, 1);
+        assert.equal(res.insertedCount, 1);
+        assert.ok(res.insertedId);
+        db.collection(id + '_collision').drop();
+        done();
+      });
+    });
+  },
+
+  'test listCollisions': function(done) {
+    const that = this;
+    MongoClient.connect(url, function(err, db) {
+      db = err ? dbStub : db;
+      var dataHandlers = defaultDataHandlersModule(db);
+      dataHandlers.handleCollision(id, metaData, {'name': 'Doe'}, function(err, res) {
+        assert.ok(!err);
+        dataHandlers.listCollisions(id, metaData, function(err, res) {
+          db.collection(id + '_collision').drop();
+          done();
+        });
+      });
+    });
+  },
+
+  'test removeCollision': function(done) {
+    const that = this;
+    MongoClient.connect(url, function(err, db) {
+      db = err ? dbStub : db;
+      var dataHandlers = defaultDataHandlersModule(db);
+      dataHandlers.handleCollision(id, metaData, {'name': 'Doe'}, function(err, res) {
+        assert.ok(!err);
+        var uid = res.insertedId;
+        dataHandlers.removeCollision(id, uid, metaData, function(err, res) {
+          assert.equal(res.result.ok, 1);
+          assert.equal(res.result.n, 1);
+          db.collection(id + '_collision').drop();
+          done();
+        });
+      });
+    });
+  }
+
+};

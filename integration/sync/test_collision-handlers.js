1975273ff00de891fef24e5664971b0be7ee9238
Aiden Keating <akeating@redhat.com>
2017-03-02 09:26:40 +0000

    FH-3198 Move custom handlers to use sinon spies
    $title

--- integration/sync/test_collision-handlers.js
+++ integration/sync/test_collision-handlers.js
@@ -1,5 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var sinon = require('sinon');
 var _ = require('underscore');
 var defaultDataHandlersModule = require('../../lib/sync/default-dataHandlers');
 var dataHandlersModule = require('../../lib/sync/dataHandlers');
@@ -9,6 +10,7 @@ var MONGODB_URL = "mongodb://127.0.0.1:27017/test";
 
 var mongodb;
 var dataHandlers;
+var defaultHandlers;
 
 function getCollisionData() {
   return {
@@ -21,98 +23,95 @@ function getCollisionData() {
 }
 
 module.exports = {
-  'before': function(done) {
-    MongoClient.connect(MONGODB_URL, function(err, db){
-      if (err) {
-        console.log('mongodb connection error', err);
+  'test collision handlers': {
+    'before': function(done) {
+      MongoClient.connect(MONGODB_URL, function(err, db){
+        if (err) {
+          console.log('mongodb connection error', err);
+          return done(err);
+        }
+        mongodb = db;
+        mongodb.dropCollection(DATASETID + '_collision');
+        defaultHandlers = defaultDataHandlersModule(mongodb);
+        dataHandlers = dataHandlersModule({
+          defaultHandlers: defaultHandlers
+        });
         return done(err);
-      }
-      mongodb = db;
+      });
+    },
+    'afterEach': function(done) {
       mongodb.dropCollection(DATASETID + '_collision');
       dataHandlers = dataHandlersModule({
         defaultHandlers: defaultDataHandlersModule(mongodb)
       });
-      return done(err);
-    });
-  },
-  'afterEach': function(done) {
-    mongodb.dropCollection(DATASETID + '_collision');
-    dataHandlers = dataHandlersModule({
-      defaultHandlers: defaultDataHandlersModule(mongodb)
-    });
-    return done();
-  },
-  'test default create and list collisions': function(done) {
-    var collisionData = getCollisionData();
-    dataHandlers.handleCollision(DATASETID, {}, collisionData, function(err, res) {
-      assert.ok(!err);
-      dataHandlers.listCollisions(DATASETID, {}, function(err, res) {
-        var collision = _.values(res)[0];
-        assert.equal(1, _.size(res));
-        assert.equal(collisionData.hash, collision.hash);
-        assert.deepEqual(collisionData, collision);
-        done(err);
-      });
-    });
-  },
-  'test default removing collisions': function(done) {
-    var collisionData = getCollisionData();
-    dataHandlers.handleCollision(DATASETID, {}, collisionData, function(err, res) {
-      dataHandlers.listCollisions(DATASETID, {}, function(err, res) {
-        var collisionId = Object.keys(res)[0];
-        var collision = _.values(res)[0];
-        assert.equal(1, _.size(res));
-        dataHandlers.removeCollision(DATASETID, collisionId, {}, function(err, res) {
-          dataHandlers.listCollisions(DATASETID, {}, function(err, res) {
-            assert.ok(!err);
-            assert.equal(0, _.size(res));
-            return done();
-          });          
-        });
-      });
-    });
-  },
-  'test custom create collision': function(done) {
-    var stringData;
-    dataHandlers.collisionHandler(DATASETID, function(datasetId, metaData, collisionData, cb) {
-      var stringData = JSON.stringify(collisionData);
-      var collision = { data: stringData };
-      mongodb.collection(DATASETID + '_collision').insertOne(collision, function(err, res) {
+      return done();
+    },
+    'test default create and list collisions': function(done) {
+      var collisionData = getCollisionData();
+      dataHandlers.handleCollision(DATASETID, {}, collisionData, function(err, res) {
         assert.ok(!err);
-        return cb(err, res.ops[0]);
-      });
-    });
-    var collisionData = getCollisionData();
-    dataHandlers.handleCollision(DATASETID, {}, collisionData, function(err, res) {
-      assert.ok(!err);
-      assert(res.data, stringData);
-      return done();
-    });
-  },
-  'test custom list collision': function(done) {
-    var stubList = ['stubbed', 'list'];
-    dataHandlers.listCollisionsHandler(DATASETID, function(datasetId, metaData, cb) {
-      return cb(null, stubList);
-    });
-    dataHandlers.listCollisions(DATASETID, {}, function(err, res) {
-      assert.ok(!err);
-      assert.deepEqual(stubList, res);
-      return done();
-    });
-  },
-  'test custom remove collision': function(done) {
-    dataHandlers.removeCollisionHandler(DATASETID, function(datasetId, dataHash, metaData, cb) {
-      return mongodb.collection(DATASETID + '_collision').remove({hash: dataHash}, cb);
-    });
-    var collisionData = getCollisionData();
-    dataHandlers.handleCollision(DATASETID, {}, collisionData, function(err, res) {
-      dataHandlers.removeCollision(DATASETID, collisionData.hash, {}, function(err, res) {
         dataHandlers.listCollisions(DATASETID, {}, function(err, res) {
-          assert.ok(!err);
-          assert.deepEqual({}, res);
-          return done();
+          var collision = _.values(res)[0];
+          assert.equal(1, _.size(res));
+          assert.equal(collisionData.hash, collision.hash);
+          assert.deepEqual(collisionData, collision);
+          done(err);
         });
       });
-    });
+    },
+    'test default removing collisions': function(done) {
+      var collisionData = getCollisionData();
+      dataHandlers.handleCollision(DATASETID, {}, collisionData, function(err, res) {
+        dataHandlers.listCollisions(DATASETID, {}, function(err, res) {
+          var collisionId = Object.keys(res)[0];
+          var collision = _.values(res)[0];
+          assert.equal(1, _.size(res));
+          dataHandlers.removeCollision(DATASETID, collisionId, {}, function(err, res) {
+            dataHandlers.listCollisions(DATASETID, {}, function(err, res) {
+              assert.ok(!err);
+              assert.equal(0, _.size(res));
+              return done();
+            });          
+          });
+        });
+      });
+    },
+    'test custom create collision': function(done) {
+      // Wrap the default handler, as long as we know a custom one is called.
+      var customHandler = sinon.spy(defaultHandlers, 'handleCollision');
+      dataHandlers.collisionHandler(DATASETID, customHandler);
+
+      var collisionData = getCollisionData();
+      dataHandlers.handleCollision(DATASETID, {}, collisionData, function(err) {
+        assert.ok(!err);
+        assert.ok(customHandler.calledOnce);
+        return done();
+      });
+    },
+    'test custom list collision': function(done) {
+      var customHandler = sinon.spy(defaultHandlers, 'listCollisions');
+      dataHandlers.listCollisionsHandler(DATASETID, customHandler);
+      dataHandlers.listCollisions(DATASETID, {}, function(err) {
+        assert.ok(!err);
+        assert.ok(customHandler.calledOnce);
+        return done();
+      });
+    },
+    'test custom remove collision': function(done) {
+      var customHandler = sinon.spy(defaultHandlers, 'removeCollision');
+      dataHandlers.removeCollisionHandler(DATASETID, customHandler);
+
+      var collisionData = getCollisionData();
+      dataHandlers.handleCollision(DATASETID, {}, collisionData, function(err, res) {
+        dataHandlers.removeCollision(DATASETID, res.uid, {}, function(err) {
+          assert.ok(!err);
+          dataHandlers.listCollisions(DATASETID, {}, function(err) {
+            assert.ok(!err);
+            assert.ok(customHandler.calledOnce);
+            return done();
+          });
+        });
+      });
+    }
   }
 };

5e70b49bafb1e169c3cb33eb5d53699168b22d03
David Martin <david.martin@redhat.com>
2017-04-12 15:00:06 +0100

    ✅ Remove real mongodb client connection from unit tests

--- test/sync/test_default-dataHandlers.js
+++ test/sync/test_default-dataHandlers.js
@@ -32,9 +32,6 @@ function resetStubUid() {
   retObject._id = stubUid;
 }
 
-var url = 'mongodb://localhost:27017/dataHandlersTest';
-var MongoClient = require('mongodb').MongoClient;
-
 module.exports = {
 
   'beforeEach': function(){
@@ -52,141 +49,117 @@ module.exports = {
   },
 
   'test doCreate': function(done) {
-    MongoClient.connect(url, function(err, db) {
-      db = err ? dbStub : db;
-      var dataHandlers = defaultDataHandlersModule(db);
-      var data = {'name': 'Fletch'};
-      dataHandlers.doCreate(id, data, metaData, function(err, res) {
-        assert.ok(!err);
-        assert.ok(res.uid);
-        assert.equal(res.data.name, 'Fletch');
-        db.collection(id).drop();
-        done();
-      });
+    var dataHandlers = defaultDataHandlersModule(dbStub);
+    var data = {'name': 'Fletch'};
+    dataHandlers.doCreate(id, data, metaData, function(err, res) {
+      assert.ok(!err);
+      assert.ok(res.uid);
+      assert.equal(res.data.name, 'Fletch');
+      dbStub.collection(id).drop();
+      done();
     });
   },
 
   'test doList': function(done) {
     const that = this;
-    MongoClient.connect(url, function(err, db) {
-      db = err ? dbStub : db;
-      var dataHandlers = defaultDataHandlersModule(db);
-      var data = {'name': 'Fletch'};
-      dataHandlers.doCreate(id, data, metaData, function(err, res) {
-        var uid = res.uid || stubUid;
-        resetStubUid();
-        dataHandlers.doList(id, queryParams, metaData, function(err, res) {
-          assert.ok(!err);
-          assert.ok(res);
-          assert.ok(res[uid]);
-          assert.equal(res[uid].name, 'Fletch');
-          db.collection(id).drop();
-          done();
-        });
+    var dataHandlers = defaultDataHandlersModule(dbStub);
+    var data = {'name': 'Fletch'};
+    dataHandlers.doCreate(id, data, metaData, function(err, res) {
+      var uid = res.uid || stubUid;
+      resetStubUid();
+      dataHandlers.doList(id, queryParams, metaData, function(err, res) {
+        assert.ok(!err);
+        assert.ok(res);
+        assert.ok(res[uid]);
+        assert.equal(res[uid].name, 'Fletch');
+        dbStub.collection(id).drop();
+        done();
       });
     });
   },
 
   'test doRead': function(done) {
     const that = this;
-    MongoClient.connect(url, function(err, db) {
-      db = err ? dbStub : db;
-      var dataHandlers = defaultDataHandlersModule(db);
-      dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
-        var uid = res.uid || stubUid;
-        dataHandlers.doRead(id, uid, metaData, function(err, res) {
-          assert.ok(!err);
-          assert.ok(!res._id);
-          db.collection(id).drop();
-          done();
-        });
+    var dataHandlers = defaultDataHandlersModule(dbStub);
+    dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
+      var uid = res.uid || stubUid;
+      dataHandlers.doRead(id, uid, metaData, function(err, res) {
+        assert.ok(!err);
+        assert.ok(!res._id);
+        dbStub.collection(id).drop();
+        done();
       });
     });
   },
 
   'test doUpdate': function(done) {
     const that = this;
-    MongoClient.connect(url, function(err, db) {
-      db = err ? dbStub : db;
-      var dataHandlers = defaultDataHandlersModule(db);
-      dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
-        var uid = res.uid || stubUid;
-        dataHandlers.doUpdate(id, uid, {name: "Fletch"}, metaData, function(err) {
-          assert.ok(!err);
-          db.collection(id).drop();
-          done();
-        });
+    var dataHandlers = defaultDataHandlersModule(dbStub);
+    dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
+      var uid = res.uid || stubUid;
+      dataHandlers.doUpdate(id, uid, {name: "Fletch"}, metaData, function(err) {
+        assert.ok(!err);
+        dbStub.collection(id).drop();
+        done();
       });
     });
   },
 
   'test doDelete': function(done) {
     const that = this;
-    MongoClient.connect(url, function(err, db) {
-      db = err ? dbStub : db;
-      var dataHandlers = defaultDataHandlersModule(db);
-      dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
-        var uid = res.uid || stubUid;
-        dataHandlers.doDelete(id, uid, metaData, function(err, res) {
-          assert.ok(!err);
-          db.collection(id).drop();
-          done();
-        });
+    var dataHandlers = defaultDataHandlersModule(dbStub);
+    dataHandlers.doCreate(id, queryParams, metaData, function(err, res) {
+      var uid = res.uid || stubUid;
+      dataHandlers.doDelete(id, uid, metaData, function(err, res) {
+        assert.ok(!err);
+        dbStub.collection(id).drop();
+        done();
       });
     });
   },
 
   'test handleCollision': function(done) {
     const that = this;
-    MongoClient.connect(url, function(err, db) {
-      db = err ? dbStub : db;
-      var dataHandlers = defaultDataHandlersModule(db);
-      var timestamp = Date.now();
-      dataHandlers.handleCollision(id, null, timestamp, null, {}, {}, metaData, function(err, res) {
-        var uid = res.uid || stubUid;
-        assert.ok(!err);
-        assert.ok(res.uid);
-        assert.ok(res.data);
-        assert.equal(res.data.timestamp, timestamp);
-        db.collection(id + '_collision').drop();
-        done();
-      });
+    var dataHandlers = defaultDataHandlersModule(dbStub);
+    var timestamp = Date.now();
+    dataHandlers.handleCollision(id, null, timestamp, null, {}, {}, metaData, function(err, res) {
+      var uid = res.uid || stubUid;
+      assert.ok(!err);
+      assert.ok(res.uid);
+      assert.ok(res.data);
+      assert.equal(res.data.timestamp, timestamp);
+      dbStub.collection(id + '_collision').drop();
+      done();
     });
   },
 
   'test listCollisions': function(done) {
     const that = this;
-    MongoClient.connect(url, function(err, db) {
-      db = err ? dbStub : db;
-      var dataHandlers = defaultDataHandlersModule(db);
-      var timestamp = Date.now();
-      dataHandlers.handleCollision(id, null, timestamp, null, {}, {}, metaData, function(err, res) {
-        var uid = res.uid || stubUid;
-        assert.ok(!err);
-        resetStubUid();
-        dataHandlers.listCollisions(id, metaData, function(err, res) {
-          assert.ok(res);
-          assert.ok(res[uid]);
-          assert.equal(res[uid].timestamp, timestamp);
-          db.collection(id + '_collision').drop();
-          done();
-        });
+    var dataHandlers = defaultDataHandlersModule(dbStub);
+    var timestamp = Date.now();
+    dataHandlers.handleCollision(id, null, timestamp, null, {}, {}, metaData, function(err, res) {
+      var uid = res.uid || stubUid;
+      assert.ok(!err);
+      resetStubUid();
+      dataHandlers.listCollisions(id, metaData, function(err, res) {
+        assert.ok(res);
+        assert.ok(res[uid]);
+        assert.equal(res[uid].timestamp, timestamp);
+        dbStub.collection(id + '_collision').drop();
+        done();
       });
     });
   },
 
   'test removeCollision': function(done) {
     const that = this;
-    MongoClient.connect(url, function(err, db) {
-      db = err ? dbStub : db;
-      var dataHandlers = defaultDataHandlersModule(db);
-      dataHandlers.handleCollision(id, null, Date.now(), null, {}, {}, metaData, function(err, res) {
-        assert.ok(!err);
-        var uid = res.insertedId;
-        dataHandlers.removeCollision(id, uid, metaData, function(err, res) {
-          db.collection(id + '_collision').drop();
-          done();
-        });
+    var dataHandlers = defaultDataHandlersModule(dbStub);
+    dataHandlers.handleCollision(id, null, Date.now(), null, {}, {}, metaData, function(err, res) {
+      assert.ok(!err);
+      var uid = res.insertedId;
+      dataHandlers.removeCollision(id, uid, metaData, function(err, res) {
+        dbStub.collection(id + '_collision').drop();
+        done();
       });
     });
   }

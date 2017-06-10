4cedb99a6088673f4f040d3a12c932498a0ed506
Wojciech Trocki <wtrocki@redhat.com>
2017-06-10 22:12:16 +0100

    Updated Typescript definitions
    https://github.com/feedhenry/feedhenry-sdk-docs/blob/master/sync_upgrade_guide.adoc

--- types/fh-sync.d.ts
+++ types/fh-sync.d.ts
@@ -64,55 +64,42 @@ declare module 'fh-sync' {
     function stop(dataset_id: string, onStop: () => void): void;
     function stopAll(onstop: StandardCb<string[]>): void;
 
-    function handleList(dataset_id: string, onList: (dataset_id: string, params: any, callback: StandardCb<any>, meta_data: any) => void): void;
-    function globalHandleList(onList: (dataset_id: string, params: any, callback: StandardCb<any>, meta_data: any) => void): void;
+    function handleList(dataset_id: string, onList: (dataset_id: string, params: any, meta_data: any, callback: StandardCb<any>) => void): void;
+    function globalHandleList(onList: (dataset_id: string, params: any,  meta_data: any, callback: StandardCb<any>) => void): void;
 
-    function handleCreate(dataset_id: string, onCreate: (dataset_id: string, data: any, callback: StandardCb<any>, meta_data: any) => void): void;
-    function globalHandleCreate(onCreate: (dataset_id: string, params: any, callback: StandardCb<any>, meta_data: any) => void): void;
+    function handleCreate(dataset_id: string, onCreate: (dataset_id: string, data: any, meta_data: any, callback: StandardCb<any>) => void): void;
+    function globalHandleCreate(onCreate: (dataset_id: string, params: any, meta_data: any, callback: StandardCb<any>) => void): void;
 
-    function handleRead(dataset_id: string, onRead: (dataset_id: string, uid: any, callback: StandardCb<any>, meta_data: any) => void): void;
-    function globalHandleRead(onRead: (dataset_id: string, uid: string, callback: StandardCb<any>, meta_data: any) => void): void;
+    function handleRead(dataset_id: string, onRead: (dataset_id: string, uid: any, meta_data: any, callback: StandardCb<any>) => void): void;
+    function globalHandleRead(onRead: (dataset_id: string, uid: string, meta_data: any, callback: StandardCb<any>) => void): void;
 
-    function handleUpdate(dataset_id: string, onUpdate: (dataset_id: string, uid: string, data: any, callback: StandardCb<any>, meta_data: any) => void): void;
-    function globalHandleUpdate(onCreate: (dataset_id: string, uid: string, data: any, callback: StandardCb<any>, meta_data: any) => void): void;
+    function handleUpdate(dataset_id: string, onUpdate: (dataset_id: string, uid: string, data: any,  meta_data: any, callback: StandardCb<any>) => void): void;
+    function globalHandleUpdate(onCreate: (dataset_id: string, uid: string, data: any, meta_data: any, callback: StandardCb<any>) => void): void;
 
-    function handleDelete(dataset_id: string, onCreate: (dataset_id: string, uid: string, callback: StandardCb<any>, meta_data: any) => void): void;
-    function globalHandleDelete(onCreate: (dataset_id: string, uid: string, callback: StandardCb<any>, meta_data: any) => void): void;
+    function handleDelete(dataset_id: string, onCreate: (dataset_id: string, uid: string, meta_data: any, callback: StandardCb<any>) => void): void;
+    function globalHandleDelete(onCreate: (dataset_id: string, uid: string, meta_data: any, callback: StandardCb<any>) => void): void;
 
-    function handleCollision(dataset_id: string, onCollision: (dataset_id: string, hash: string, timestamp: TODO, uid: string, pre: any, post: any, meta_data: any) => void): void;
-    function globalHandleCollision(onCollision: (dataset_id: string, hash: string, timestamp: Date, uid: string, pre: any, post: any, meta_data: any) => void): void;
+    function handleCollision(dataset_id: string, onCollision: (dataset_id: string, hash: string, timestamp: TODO, uid: string, pre: any, post: any, meta_data: any, callback: StandardCb<any>) => void): void;
+    function globalHandleCollision(onCollision: (dataset_id: string, hash: string, timestamp: Date, uid: string, pre: any, post: any, meta_data: any, callback: StandardCb<any>) => void): void;
 
-    function listCollisions(dataset_id: string, onList: (dataset_id: string, callback: StandardCb<{ [hash: string]: any }>, meta_data: any) => void): void;
-    function globalListCollisions(onList: (dataset_id: string, callback: StandardCb<{ [hash: string]: any }>, meta_data: any) => void): void;
+    function listCollisions(dataset_id: string, onList: (dataset_id: string, callback: StandardCb<{ [hash: string]: any }>, meta_data: any, callback: StandardCb<any>) => void): void;
+    function globalListCollisions(onList: (dataset_id: string, callback: StandardCb<{ [hash: string]: any }>, meta_data: any, callback: StandardCb<any>) => void): void;
 
-    function removeCollision(dataset_id: string, onRemove: (dataset_id: string, collision_hash: string, callback: StandardCb<TODO>, meta_data: any) => void): void;
-    function globalListCollisions(onRemove: (dataset_id: string, collision_hash: string, callback: StandardCb<TODO>, meta_data: any) => void): void;
+    function removeCollision(dataset_id: string, onRemove: (dataset_id: string, collision_hash: string, callback: StandardCb<TODO>, meta_data: any, callback: StandardCb<any>) => void): void;
+    function globalListCollisions(onRemove: (dataset_id: string, collision_hash: string, callback: StandardCb<TODO>, meta_data: any, callback: StandardCb<any>) => void): void;
 
     function interceptRequest(dataset_id: string, onIntercept: (dataset_id: string, interceptor_params: SyncInterceptParams, callback: NoRespCb) => void): void;
+    function interceptResponse(): void;
 
-    function interceptResponse();
+    function setConfig(config:any): void;
+    
+    function globalInterceptRequest(onIntercept: (dataset_id: string, interceptor_params: SyncInterceptParams, callback: NoRespCb) => void): void;
+    function globalInterceptResponse(): void;
+    
+    function setGlobalHashFn((object: any)=> any): void;
 
-    function globalListCollisions(onIntercept: (dataset_id: string, interceptor_params: SyncInterceptParams, callback: NoRespCb) => void): void;
-
-    function start(cb: TODO)
-    function stop(cb: TODO)
-
-    function setConfig(config:any)
-    function globalInterceptRequest();
-    function globalInterceptResponse();
-    function setRecordHashFn();
-    function setGlobalHashFn();
-    function getEventEmitter();
-    function setEventEmitter();
-
-    /**
-     * Legacy function please do not use.
-     * @deprecated
-     * @param dataset_id 
-     * @param returnData 
-     * @param cb 
-     */
-    function toJSON(dataset_id: string, returnData: any, cb: (err: Error, data: any) => void)
+    // Legacy function please do not use.
+    function toJSON(dataset_id: string, returnData: any, cb: (err: Error, data: any) => void): void;
   }
   export = Sync;
 }

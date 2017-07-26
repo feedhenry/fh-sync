0cae4430b61ae1665746a511985ac0dc69d7d950
Wei Li <wei.li@redhat.com>
2017-07-26 13:20:42 +0100

    ✨ adding a new configuration option to allow developer to override the  cuid field

--- integration/test_sync_apis.js
+++ integration/test_sync_apis.js
@@ -26,7 +26,11 @@ module.exports = {
         ackWorkerInterval: 100, 
         schedulerInterval: 100, 
         schedulerLockName: 'test:syncApi:lock', 
-        useCache: true});
+        useCache: true,
+        cuidGenerator: function(params) {
+          return params.__fh.cuid + params.query_params.user;
+        }
+    });
       async.series([
         async.apply(sync.api.connect, mongoDBUrl, null, redisUrl),
         async.apply(sync.api.init, DATASETID, {syncFrequency: 1}),
